cacd70238af2318c68dca224eb91387e09cfccf5
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-02-29 02:14:21 -0400

    se puede saludar por edad

--- src/saludador.js
+++ src/saludador.js
@@ -12,13 +12,40 @@ function saludar(nombre, genero) {
   {
     saludo = "";
   }
-  if(genero == 'F')
-  {
-    saludo = saludo + "Sra." + nombre;
-  }else{
-    if(genero == 'M')
+  else{
+    if(genero == 'F')
     {
-      saludo = saludo + "Sr." + nombre;
+      saludo = saludo + "Sra." + nombre;
+    }else{
+      if(genero == 'M')
+      {
+        saludo = saludo + "Sr." + nombre;
+      }
+    }
+  }
+  return saludo 
+}
+
+function saludar(nombre, genero, edad) {
+  let saludo = "Hola ";
+  if(nombre == "" || genero == "" || edad == "")
+  {
+    saludo = "";
+  }else
+  {
+    if(edad>30)
+    {
+      if(genero == 'F')
+      {
+        saludo = saludo + "Sra." + nombre;
+      }else{
+        if(genero == 'M')
+        {
+          saludo = saludo + "Sr." + nombre;
+        }
+      }
+    }else{
+      saludo = saludo + "Joven " + nombre;
     }
   }
   return saludo 
