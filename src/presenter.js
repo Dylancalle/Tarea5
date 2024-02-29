c8a3b859a532975e02c752a67fd8e7b345bef827
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-02-29 02:00:45 -0400

    se agrego el saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -2,12 +2,14 @@ import saludar from "./saludador.js"
 
 const form = document.querySelector("#saludar-form");
 const nombre_in = document.querySelector("#nombre");
+const genero_in = document.querySelector("#genero");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_in.value;
+  const genero = genero_in.value;
 
-  div.innerHTML = "<p>" + saludar(nombre) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, genero) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -6,4 +6,22 @@ function saludar(nombre) {
   return "Hola " + nombre;
 }
 
+function saludar(nombre, genero) {
+  let saludo = "Hola ";
+  if(nombre == "" || genero == "")
+  {
+    saludo = "";
+  }
+  if(genero == 'F')
+  {
+    saludo = saludo + "Sra." + nombre;
+  }else{
+    if(genero == 'M')
+    {
+      saludo = saludo + "Sr." + nombre;
+    }
+  }
+  return saludo 
+}
+
 export default saludar;
